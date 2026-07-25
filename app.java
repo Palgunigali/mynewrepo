1ba4612a27a07841c1aed1358c20b5c9b990563c
Palgunigali <palgunigali99@gmail.com>
2026-07-25 22:26:19 +0530

    Create app.java

---
+++ app.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        int num1 = 10;
+        int num2 = 20;
+        
+        // Calculate the sum using the + operator
+        int sum = num1 + num2;
+        
+        // Print the result
+        System.out.println("The sum is: " + sum);
+    }
+}
